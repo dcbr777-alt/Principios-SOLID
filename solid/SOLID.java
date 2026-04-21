56ef4c28388bb2224e87b175a12fa22ecc8075fa
DavidCbr <dcbr777@gmail.com>
2026-04-21 04:40:34 -0500

    Add SOLID principles implementation in SOLID.java

---
+++ solid/SOLID.java
@@ -0,0 +1,18 @@
+// SRP - Separación de responsabilidades
+
+class Proyecto {
+    public void agregarTarea() {}
+    public void calcularAvance() {}
+}
+
+class ReporteProyecto {
+    public void generarReporte() {}
+}
+
+class NotificadorProyecto {
+    public void enviarEmail() {}
+}
+
+class RepositorioProyecto {
+    public void guardar() {}
+}
